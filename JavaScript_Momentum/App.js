92e82f30064189c2fad9ed68901070bafc8f8107
Kangdy <canfrog@naver.com>
2021-12-16 23:39:41 +0900

    Make Login Button

--- JavaScript_Momentum/App.js
+++ JavaScript_Momentum/App.js
@@ -1,14 +1,10 @@
-const h1 = document.querySelector(".hello h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+
+function onLoginBtnClick() {
+    const username = loginInput.value;
+    console.log(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
-
-
+loginButton.addEventListener("click", onLoginBtnClick);
