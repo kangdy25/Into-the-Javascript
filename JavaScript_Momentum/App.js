6bc3d44340dc87d9cf6b2b901ae2456a893d33ee
Kangdy <canfrog@naver.com>
2021-12-19 12:04:40 +0900

    Learn PreventDefault()

--- JavaScript_Momentum/App.js
+++ JavaScript_Momentum/App.js
@@ -1,10 +1,19 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
+const link = document.querySelector("a");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    console.log(username);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+    // const username = loginInput.value;
+    // console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
