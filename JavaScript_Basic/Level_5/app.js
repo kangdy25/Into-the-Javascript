b9ace1e0f5478fd93b4bbc5b6f2ec2ee8d90dc74
Kangdy <canfrog@naver.com>
2024-01-27 23:15:19 +0900

    Learn Promise

--- JavaScript_Basic/Level_5/app.js
+++ JavaScript_Basic/Level_5/app.js
@@ -11,3 +11,28 @@ var age = 30;
 
 var obj = { name : name, age : age };
 var obj = { name, age }
+
+// Promise
+let promise1 =  new Promise(function(success, fail){
+    let operator = 1 + 1;
+    success(operator); // then() 실행
+    // fail(); // catch() 실행
+}); // 성공/실패 판정 기계
+
+let promise2 = new Promise((success, fail)=>{
+    setTimeout(()=>{
+        success();
+    }, 5000);
+})
+
+promise1.then(function(result){
+    console.log('앙 성공띠 ' + result);
+}).catch(function(){
+    console.log('힝 실패 ㅠㅠ')
+});
+
+promise2.then(function(){
+    console.log('앙 성공띠 ');
+}).catch(function(){
+    console.log('힝 실패 ㅠㅠ')
+});
--- JavaScript_Basic/Level_5/level_5_ps.js
+++ JavaScript_Basic/Level_5/level_5_ps.js
@@ -22,3 +22,49 @@ let {
     },
     size : [up, down]
 } = bodyData
+
+////////////// Promise //////////////
+
+// Q1. <img> 이미지 로딩 성공시 특정 코드를 실행하고 싶습니다. 
+let image = document.querySelector("#test");
+
+let loading = new Promise((success, fail)=>{
+    image.addEventListener('load', ()=>{
+        success();
+    });
+    image.addEventListener('error', ()=>{
+        fail();
+    });
+});
+
+loading.then(()=>{
+    console.log('성공')
+}).catch(()=>{
+    console.log('실패')
+})
+
+// Q2. Ajax 요청이 성공하면 무언가 코드를 실행하고 싶습니다. 
+
+let promise = new Promise((success, fail)=>{
+    $.ajax({
+        type : 'GET',
+        url : 'https://codingapple1.github.io/hello.txt'
+    }).done((data)=>{
+        success(data)
+    })
+})
+
+promise.then((data)=>{
+    console.log(data);
+    let nextPromise = new Promise((success, fail)=>{
+        $.ajax({
+            type : 'GET',
+            url : 'https://codingapple1.github.io/hello2.txt'
+        }).done((data2)=>{
+            success(data2)
+        })
+    })
+    return nextPromise;
+}).then((data2)=>{
+    console.log(data2)
+})
