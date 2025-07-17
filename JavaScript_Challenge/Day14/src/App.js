2b041117e52910f34a1933d38af1479e9c5070d8
Kangdy <canfrog@naver.com>
2025-07-17 23:03:09 +0900

    Challenge Day 14

--- JavaScript_Challenge/Day14/src/App.js
+++ JavaScript_Challenge/Day14/src/App.js
@@ -1,8 +1,9 @@
 import Header from './components/Header.js'
 import PokemonList from './components/PokemonList.js';
+import PokemonDetail from './components/PokemonDetail.js'
 
 //APIS
-import { getPokemonList } from './modules/api.js';
+import { getPokemonList, getPokemonDetail } from './modules/api.js';
 
 export default function App($app) {
     const getSearchWord = () => {
@@ -13,94 +14,135 @@ export default function App($app) {
     };
 
     this.state = {
-        type: '',
+        type: window.location.pathname.replace("/", ""),
         pokemonList: [],
         searchWord: getSearchWord(),
         currentPage: window.location.pathname,
     };
 
-    const header = new Header({
-        //코드 작성
-        $app,
-        initialState: {
-            currentPage: this.state.currentPage,
-            searchWord: this.state.searchWord,
-        },
-        //'포켓몬 도감'을 클릭하면 "/" 홈으로 돌아갈 수 있도록 함수를 완성하세요.
-        handleClick: async () => {
-            history.pushState(null, null, `/`);
-            const pokemonList = await getPokemonList();
-            this.setState({
-                ...this.state,
-                pokemonList: pokemonList,
-                type: '',
-                searchWord: getSearchWord(),
-                currentPage: "/"
+    const renderHeader = () => {
+        new Header({
+            $app,
+            initialState: {
+                currentPage: this.state.currentPage,
+                searchWord: this.state.searchWord,
+            },
+            handleClick: async () => {
+                history.pushState(null, null, `/`);
+                const pokemonList = await getPokemonList();
+                this.setState({
+                    ...this.state,
+                    pokemonList: pokemonList,
+                    type: '',
+                    searchWord: getSearchWord(),
+                    currentPage: "/"
+                })
+            },
+            handleSearch: async (searchWord) => {
+                history.pushState(null, null, `?search=${searchWord}`);
+                const searchedPokemonList = await getPokemonList(this.state.type, searchWord);
+                this.setState({
+                    ...this.state,
+                    pokemonList: searchedPokemonList,
+                    searchWord: searchWord,
+                    currentPage: `?search=${searchWord}`
+                })
+            },
+        })
+    }
+
+    const renderPokemonList = () => {
+        new PokemonList({
+            $app,
+            initialState: this.state.pokemonList,
+            handleItemClick: (id) => {
+                history.pushState(null, null, `/detail/${id}`);
+                this.setState({
+                    ...this.state,
+                    currentPage: `/detail/${id}`,
+                });
+            },
+            handleTypeClick: async (type) => {
+                history.pushState(null, null, `/${type}`);
+                const pokemonList = await getPokemonList(type);
+                this.setState({
+                    ...this.state,
+                    pokemonList: pokemonList,
+                    searchWord: getSearchWord(),
+                    type: type,
+                    currentPage: `/${type}`,
+                });
+            },
+        });
+    } 
+
+    const renderPokemonDetail = async (id) => {
+        try {
+            const pokemonDetailData = await getPokemonDetail(id);
+            console.log(pokemonDetailData)
+            new PokemonDetail({
+                $app,
+                initialState: pokemonDetailData,
             })
-        },
-        //'돋보기 모양'을 누르면 검색 결과를 나타내고, "(기존 url)/?search=searchWord"로 url을 변경하세요.
-        handleSearch: async (searchWord) => {
-            history.pushState(null, null, `?search=${searchWord}`);
-            const searchedPokemonList = await getPokemonList(this.state.type, searchWord);
-            this.setState({
-                ...this.state,
-                pokemonList: searchedPokemonList,
-                searchWord: searchWord,
-                currentPage: `?search=${searchWord}`
-            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
-        },
-    });
+    const render = async () => {
+        const path = this.state.currentPage;
+        $app.innerHTML = '';
 
-    const pokemonList = new PokemonList({
-        // 코드 작성
-        $app,
-        initialState: this.state.pokemonList,
-        //아이템을 클릭하면 "/detail/id" 로 이동할 수 있도록 아래의 함수를 완성하세요.
-        handleItemClick: (id) => {
-            history.pushState(null, null, `/detail/${id}`);
-            this.setState({
-                ...this.state,
-                currentPage: `/detail/${id}`,
-            });
-        },
-
-        //타입을 클릭하면, 클리한 타입에 해당하는 포켓몬만 띄워지고,
-        // "/type" 으로 이동할 수 있도록 아래의 함수를 완성하세요.
-        handleTypeClick: async (type) => {
-            history.pushState(null, null, `/${type}`);
-            const pokemonList = await getPokemonList(type);
-            this.setState({
-                ...this.state,
-                pokemonList: pokemonList,
-                searchWord: getSearchWord(),
-                type: type,
-                currentPage: `/${type}`,
-            });
-        },
-    });
+        if (!path.startsWith('/detail')) {
+            renderHeader();
+            renderPokemonList();
+        } else {
+            const pokemonId = path.split('/detail/')[1];
+            renderHeader();
+            renderPokemonDetail(pokemonId);
+        }
+    }
 
     this.setState = (newState) => {
         this.state = newState;
-        // 코드 작성
-        header.setState({
-            searchWord: this.state.searchWord,
-            currentPage: this.state.currentPage,
-        })
-        pokemonList.setState(this.state.pokemonList);
+        render();
     };
 
     const init = async () => {
-        try {
-            const initialPokemonList = await getPokemonList();
-            this.setState({
-                ...this.state,
-                pokemonList: initialPokemonList,
-            });
-        } catch (err) {
-            console.log(err);
+        const path = this.state.currentPage;
+
+        if (!path.startsWith("/detail")) {
+            try {
+                const initialPokemonList = await getPokemonList(
+                    this.state.type,
+                    this.state.searchWord
+                );
+                this.setState({
+                    ...this.state,
+                    pokemonList: initialPokemonList,
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            render();
         }
     };
+
+    window.addEventListener('popstate', async () => {
+        const urlPath = window.location.pathname;
+        const prevType = urlPath.replace("/", "");
+        const prevSearchWord = getSearchWord();
+        const prevPokemonList = await getPokemonList(prevType, prevSearchWord);
+
+        this.setState({
+        ...this.state,
+        type: prevType,
+        pokemonList: prevPokemonList,
+        searchWord: prevSearchWord,
+        currentPage: urlPath,
+        });
+    });
 
     init();
 }
